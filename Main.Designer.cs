a08e3faf04f3263dbf1b1108c6186d9c4cc9591c
Julie Newman <julienewman@usf.edu>
2022-02-22 21:37:24 -0500

    Initial Project creation

--- Main.Designer.cs
+++ Main.Designer.cs
@@ -57,6 +57,7 @@
             this.submitButton.TabIndex = 2;
             this.submitButton.Text = "Submit";
             this.submitButton.UseVisualStyleBackColor = true;
+            this.submitButton.Click += new System.EventHandler(this.submitButton_Click);
             // 
             // Main
             // 
--- Main.cs
+++ Main.cs
@@ -16,5 +16,10 @@ namespace FoxTrotTeamProject
         {
             InitializeComponent();
         }
+
+        private void submitButton_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
